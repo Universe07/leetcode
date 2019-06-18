a736283f0525d45653f8f2f5649339fc32d113a0
Universe07 <han.li@kcl.ac.uk>
2019-06-18 15:52:22 +0800

    92_reverse_linked_list_II.cpp

--- 92_reverse_linked_list_II.cpp
+++ 92_reverse_linked_list_II.cpp
@@ -1,23 +1,17 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+// Time: O(n)
+// Space: O(1)
 class Solution {
  public:
   ListNode* reverseBetween(ListNode* head, int m, int n) {
     ListNode dummy(0);
     dummy.next = head;
     ListNode* prev = &dummy;
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m - 1; ++i) {
       prev = prev->next;
     }
     auto head2 = prev;
-    ListNode* rest = prev->next;
     prev = prev->next;
+    ListNode* rest = prev->next;
 
     for (int i = 0; i < n - m; ++i) {
       prev->next = rest->next;
