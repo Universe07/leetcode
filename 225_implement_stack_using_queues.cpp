2f57ef225d5e31e0d8573cad04d3a26beae1e7a0
Universe07 <han.li@kcl.ac.uk>
2019-06-23 10:22:04 +0800

    225_implement_stack_using_queues.cpp

---
+++ 225_implement_stack_using_queues.cpp
@@ -0,0 +1,39 @@
+class MyStack {
+ public:
+  /** Initialize your data structure here. */
+  MyStack() {}
+
+  /** Push element x onto stack. */
+  void push(int x) {
+    que.emplace(x);
+    for (int i = 0; i < que.size() - 1; ++i) {
+      que.emplace(que.front());
+      que.pop();
+    }
+  }
+
+  /** Removes the element on top of the stack and returns that element. */
+  int pop() {
+    int front = que.front();
+    que.pop();
+    return front;
+  }
+
+  /** Get the top element. */
+  int top() { return que.front(); }
+
+  /** Returns whether the stack is empty. */
+  bool empty() { return que.empty(); }
+
+ private:
+  queue<int> que;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
