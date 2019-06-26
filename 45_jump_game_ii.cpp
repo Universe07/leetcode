57d879e4fd1e6eebb535f510967533081779cb3d
Universe07 <han.li@kcl.ac.uk>
2019-06-26 21:46:48 +0800

    45_jump_game_ii.cpp

---
+++ 45_jump_game_ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  int jump(vector<int>& nums) {
+    int ans = 0;
+    int cur = 0, pre = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      if (i > pre) {
+        pre = cur;
+        ++ans;
+      }
+      cur = max(cur, i + nums[i]);
+    }
+    return ans;
+  }
+};
