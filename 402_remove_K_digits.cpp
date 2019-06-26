87b03feb15757d80f34c90fe9d1c6c8f5c033982
Universe07 <han.li@kcl.ac.uk>
2019-06-26 11:20:56 +0800

    402_remove_K_digits.cpp

---
+++ 402_remove_K_digits.cpp
@@ -0,0 +1,23 @@
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+ public:
+  string removeKdigits(string num, int k) {
+    string res;
+    for (const auto& c : num) {
+      while (!res.empty() && res.back() > c && k > 0) {
+        res.pop_back();
+        --k;
+      }
+      res.push_back(c);
+    }
+    res.resize(res.size() - k);
+    auto it = res.find_first_not_of('0');
+    if (it == string::npos || res == "0") {
+      return "0";
+    } else {
+      return res.substr(it);
+    }
+  }
+};
