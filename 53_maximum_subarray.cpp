1a999f2d97b496c7234aeb96524c895bf25ca22d
Universe07 <han.li@kcl.ac.uk>
2019-06-27 10:40:02 +0800

    53_maximum_subarray.cpp

---
+++ 53_maximum_subarray.cpp
@@ -0,0 +1,14 @@
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+ public:
+  int maxSubArray(vector<int>& nums) {
+    auto max_sum = INT_MIN, sum = 0;
+    for (const auto& num : nums) {
+      sum = max(sum + num, sum);
+      max_sum = max(max_sum, sum);
+    }
+    return max_sum;
+  }
+};
