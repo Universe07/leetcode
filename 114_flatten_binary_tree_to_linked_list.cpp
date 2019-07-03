dbd5600cff589a546465930b93ae48f8bf411baf
Universe07 <han.li@kcl.ac.uk>
2019-07-03 17:14:00 +0800

    114_flatten_binary_tree_to_linked_list.cpp

--- 114_flatten_binary_tree_to_linked_list.cpp
+++ 114_flatten_binary_tree_to_linked_list.cpp
@@ -26,3 +26,39 @@ class Solution {
  private:
   TreeNode* p;
 };
+
+class Solution {
+ public:
+  void flatten(TreeNode* root) {
+    TreeNode* last = nullptr;
+    preorder(root, last);
+  }
+
+ private:
+  void preorder(TreeNode* node, TreeNode*& last) {
+    if (!node) {
+      return;
+    }
+    if (!node->left && !node->right) {
+      last = node;
+      return;
+    }
+    TreeNode* left = node->left;
+    TreeNode* right = node->right;
+    TreeNode* left_last = nullptr;
+    TreeNode* right_last = nullptr;
+    if (left) {
+      preorder(left, left_last);
+      node->left = nullptr;
+      node->right = left;
+      last = left_last;
+    }
+    if (right) {
+      preorder(right, right_last);
+      if (left_last) {
+        left_last->right = right;
+      }
+      last = right_last;
+    }
+  }
+};
