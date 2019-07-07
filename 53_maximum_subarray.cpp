beaacd1d98dab7e122144e4fb976d31a32136bb3
Universe07 <han.li@kcl.ac.uk>
2019-07-07 14:58:11 +0800

    53_maximum_subarray.cpp

--- 53_maximum_subarray.cpp
+++ 53_maximum_subarray.cpp
@@ -6,7 +6,7 @@ class Solution {
   int maxSubArray(vector<int>& nums) {
     auto max_sum = INT_MIN, sum = 0;
     for (const auto& num : nums) {
-      sum = max(sum + num, sum);
+      sum = max(sum + num, num);
       max_sum = max(max_sum, sum);
     }
     return max_sum;
