01ba876e7aaacb635c509915c49d88e1fe062c6d
Universe07 <han.li@kcl.ac.uk>
2019-07-10 10:20:51 +0800

    253_meeting_rooms_ii.cpp

---
+++ 253_meeting_rooms_ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  int minMeetingRooms(vector<vector<int>>& intervals) {
+    map<int, int> lookup;
+    for (const auto& i : intervals) {
+      ++lookup[i[0]];
+      --lookup[i[1]];
+    }
+    int result = 0, curr = 0;
+    for (const auto& [time, num] : lookup) {
+      result = max(result, curr += num);
+    }
+    return result;
+  }
+};
