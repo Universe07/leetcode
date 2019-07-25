5030dbc7bbccb0be583db48e7bf0fb354b35e982
Universe07 <han.li@kcl.ac.uk>
2019-07-25 17:10:35 +0800

    394_decode_string.cpp

---
+++ 394_decode_string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+ public:
+  string decodeString(string s) {
+    string curr;
+    stack<int> nums;
+    stack<string> strs;
+    int n = 0;
+    for (const auto& c : s) {
+      if (isdigit(c)) {
+        n = n * 10 + c - '0';
+      } else if (c == '[') {
+        nums.push(n);
+        strs.emplace(curr);
+        n = 0;
+        curr.clear();
+      } else if (c == ']') {
+        for (; nums.top() > 0; nums.top()--) {
+          strs.top() += curr;
+        }
+        nums.pop();
+        curr = strs.top();
+        strs.pop();
+      } else {
+        curr += c;
+      }
+    }
+    return strs.empty() ? curr : strs.top();
+  }
+};
