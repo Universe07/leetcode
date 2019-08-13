f3ac708775404206740ee0e53eacccf5d854a743
Universe07 <han.li@kcl.ac.uk>
2019-08-13 18:53:43 +0800

    111_minimum_depth_of_binary_tree.cpp

--- 111_minimum_depth_of_binary_tree.cpp
+++ 111_minimum_depth_of_binary_tree.cpp
@@ -19,3 +19,36 @@ class Solution {
     return min(l, r) + 1;
   }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  int minDepth(TreeNode* root) {
+    if (!root) return 0;
+    int level = 0;
+    queue<TreeNode*> que;
+    que.push(root);
+    while (!que.empty()) {
+      level++;
+      int size = que.size();
+      for (int i = 0; i < size; ++i) {
+        auto node = que.front();
+        que.pop();
+        if (node->left) que.push(node->left);
+        if (node->right) que.push(node->right);
+        if (!node->left && !node->right) {
+          return level;
+        }
+      }
+    }
+    return -1;
+  }
+};
