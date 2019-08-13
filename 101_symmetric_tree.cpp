9e63b8f37e36ce57afe526396413c36993933f7b
Universe07 <han.li@kcl.ac.uk>
2019-08-13 17:18:26 +0800

    101_symmetric_tree.cpp

--- 101_symmetric_tree.cpp
+++ 101_symmetric_tree.cpp
@@ -26,3 +26,30 @@ class Solution {
            isSymmetricHelper(left->right, right->left);
   }
 };
+
+class Solution {
+ public:
+  bool isSymmetric(TreeNode* root) {
+    if (!root) return true;
+    stack<TreeNode*> nodes;
+    nodes.emplace(root->left);
+    nodes.emplace(root->right);
+    while (!nodes.empty()) {
+      auto right = nodes.top();
+      nodes.pop();
+      auto left = nodes.top();
+      nodes.pop();
+      if (!left && !right) {
+        continue;
+      }
+      if (!left || !right || left->val != right->val) {
+        return false;
+      }
+      nodes.push(left->left);
+      nodes.push(right->right);
+      nodes.push(left->right);
+      nodes.push(right->left);
+    }
+    return true;
+  }
+};
