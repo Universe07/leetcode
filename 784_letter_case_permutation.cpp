4293edafe031f46d25b63914fb9db86346aeb941
Universe07 <han.li@kcl.ac.uk>
2019-08-19 16:30:32 +0800

    784_letter_case_permutation.cpp

--- 784_letter_case_permutation.cpp
+++ 784_letter_case_permutation.cpp
@@ -1,21 +1,20 @@
 class Solution {
  public:
   vector<string> letterCasePermutation(string S) {
-    vector<string> ans{""};
-    for (const auto& c : S) {
-      if (isalpha(c)) {
-        const auto& size = ans.size();
-        for (int i = 0; i < ans.size(); ++i) {
-          ans.emplace_back(ans[i]);
-          ans[i] += tolower[c];
-          ans.back() += toupper(c);
-        }
-      } else {
-        for (const auto& s : ans) {
-          s += c;
-        }
-      }
+    vector<string> ans;
+    dfs(S, 0, ans);
+    return ans;
+  }
+  void dfs(string& S, int pos, vector<string>& ans) {
+    if (pos == S.length()) {
+      ans.push_back(S);
+      return;
     }
-    return result;
+    dfs(S, pos + 1, ans);
+    if (!isdigit(S[pos])) {
+      S[pos] ^= (1 << 5);
+      dfs(S, pos + 1, ans);
+      S[pos] ^= (1 << 5);
+    }
   }
 };
