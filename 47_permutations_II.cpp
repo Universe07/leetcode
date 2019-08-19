e80ec3c38d1696d6be93ba3378687f920c951cec
Universe07 <han.li@kcl.ac.uk>
2019-08-19 15:57:26 +0800

    47_permutations_II.cpp

--- 47_permutations_II.cpp
+++ 47_permutations_II.cpp
@@ -32,3 +32,31 @@ class Solution {
     }
   }
 };
+
+class Solution {
+ public:
+  vector<vector<int>> permuteUnique(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    vector<int> used(nums.size());
+    vector<int> tmp;
+    dfs(nums, used, tmp, ans);
+    return ans;
+  }
+  void dfs(const vector<int>& nums, vector<int>& used, vector<int>& tmp,
+           vector<vector<int>>& ans) {
+    if (tmp.size() == nums.size()) {
+      ans.push_back(tmp);
+      return;
+    }
+    for (int i = 0; i < nums.size(); ++i) {
+      if (used[i]) continue;
+      if (i > 0 && nums[i - 1] == nums[i] && !used[i - 1]) continue;
+      used[i] = 1;
+      tmp.push_back(nums[i]);
+      dfs(nums, used, tmp, ans);
+      tmp.pop_back();
+      used[i] = 0;
+    }
+  }
+};
