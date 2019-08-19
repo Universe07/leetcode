481b24a1bd8864dabdab2fd0b1598341c0f58c6f
Universe07 <han.li@kcl.ac.uk>
2019-08-19 18:26:09 +0800

    22_generate_parentheses.cpp

--- 22_generate_parentheses.cpp
+++ 22_generate_parentheses.cpp
@@ -15,3 +15,28 @@ class Solution {
     if (l > r) generate(item + ')', ans, n, l, r + 1);
   }
 };
+
+class Solution {
+ public:
+  vector<string> generateParenthesis(int n) {
+    vector<string> ans;
+    string cur;
+    dfs(n, n, cur, ans);
+    return ans;
+  }
+  void dfs(int left, int right, string& cur, vector<string>& ans) {
+    if (left + right == 0) {
+      ans.push_back(cur);
+      return;
+    }
+    if (r < l) return;
+    if (l > 0) {
+      dfs(left - 1, right, cur += '(', ans);
+      cur.pop_back();
+    }
+    if (r > 0) {
+      dfs(left, right - 1, cur += ')', ans);
+      cur.pop_back();
+    }
+  }
+};
