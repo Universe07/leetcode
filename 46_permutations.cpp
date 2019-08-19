0f897f46b8280947b61aaee12cfef5c363987a5d
Universe07 <han.li@kcl.ac.uk>
2019-08-19 15:22:37 +0800

    46_permutations.cpp

--- 46_permutations.cpp
+++ 46_permutations.cpp
@@ -16,3 +16,30 @@ class Solution {
     }
   }
 };
+
+class Solution {
+ public:
+  vector<vector<int>> permute(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    vector<int> used(nums.size());
+    vector<int> tmp;
+    dfs(nums, used, tmp, ans);
+    return ans;
+  }
+  void dfs(const vector<int>& nums, vector<int>& used, vector<int>& tmp,
+           vector<vector<int>>& ans) {
+    if (tmp.size() == nums.size()) {
+      ans.push_back(tmp);
+      return;
+    }
+    for (int i = 0; i < nums.size(); ++i) {
+      if (used[i]) continue;
+      used[i] = 1;
+      tmp.push_back(nums[i]);
+      dfs(nums, used, tmp, ans);
+      tmp.pop_back();
+      used[i] = 0;
+    }
+  }
+};
