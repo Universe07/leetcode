e45390fec3d1790464bd940e7db08994eec119b7
Universe07 <han.li@kcl.ac.uk>
2019-08-23 15:40:52 +0800

    79_word_search.cpp

--- 79_word_search.cpp
+++ 79_word_search.cpp
@@ -3,31 +3,30 @@ class Solution {
   bool exist(vector<vector<char>>& board, string word) {
     const int m = board.size();
     const int n = board[0].size();
-    vector<vector<bool>> visited(m, vector<bool>(n, false));
     for (int i = 0; i < m; ++i) {
       for (int j = 0; j < n; ++j) {
-        if (dfs(board, word, 0, i, j, visited)) {
+        if (dfs(board, word, 0, i, j)) {
           return true;
         }
       }
     }
     return false;
   }
-  bool dfs(vector<vector<char>>& board, string word, int pos, int i, int j,
-           vector<vector<bool>>& visited) {
-    if (pos == word.size()) {
-      return true;
-    }
-    if (i < 0 || i >= visited.size() || j < 0 || j >= visited[0].size() ||
-        visited[i][j] || board[i][j] != word[pos]) {
+  bool dfs(vector<vector<char>>& board, string word, int pos, int i, int j) {
+    if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() ||
+        board[i][j] != word[pos]) {
       return false;
     }
-    visited[i][j] = true;
-    bool ret = dfs(board, word, pos + 1, i + 1, j, visited) ||
-               dfs(board, word, pos + 1, i - 1, j, visited) ||
-               dfs(board, word, pos + 1, i, j + 1, visited) ||
-               dfs(board, word, pos + 1, i, j - 1, visited);
-    visited[i][j] = false;
+    if (pos == word.size() - 1) {
+      return true;
+    }
+    char cur = board[i][j];
+    board[i][j] = 0;
+    bool ret = dfs(board, word, pos + 1, i + 1, j) ||
+               dfs(board, word, pos + 1, i - 1, j) ||
+               dfs(board, word, pos + 1, i, j + 1) ||
+               dfs(board, word, pos + 1, i, j - 1);
+    board[i][j] = cur;
     return ret;
   }
 };
