42870708994354d45742f63d2d354f8d2324a068
Universe07 <han.li@kcl.ac.uk>
2019-08-29 18:00:08 +0800

    542_01_matrix.cpp

--- 542_01_matrix.cpp
+++ 542_01_matrix.cpp
@@ -30,3 +30,44 @@ class Solution {
     return ans;
   }
 };
+
+class Solution {
+ public:
+  vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+    const int m = matrix.size();
+    const int n = matrix[0].size();
+    queue<pair<int, int>> que;
+    vector<vector<int>> visited(m, vector<int>(n, 0));
+    vector<vector<int>> ans(m, vector<int>(n, INT_MAX));
+
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (matrix[i][j] == 0) {
+          que.push({i, j});
+          visited[i][j] = 1;
+        }
+      }
+    }
+    vector<int> dirs = {0, -1, 0, 1, 0};
+    int step = 0;
+    while (!que.empty()) {
+      int size = que.size();
+      while (size--) {
+        auto pair = que.front();
+        que.pop();
+        int i = pair.first;
+        int j = pair.second;
+        ans[i][j] = step;
+        for (int k = 0; k < 4; k++) {
+          int x = i + dirs[k];
+          int y = j + dirs[k + 1];
+          if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y]) continue;
+          que.push({x, y});
+          visited[x][y] = 1;
+        }
+      }
+      ++step;
+    }
+    return ans;
+  }
+};
