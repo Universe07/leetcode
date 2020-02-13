cf740f40850e3d51e059a50a34144248b7c58399
Universe07 <han.li@kcl.ac.uk>
2020-02-13 19:42:26 +0800

    530_minimum_absolute_difference_in_BST.cpp

--- 530_minimum_absolute_difference_in_BST.cpp
+++ 530_minimum_absolute_difference_in_BST.cpp
@@ -7,7 +7,47 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// Space: O(n)
+// Time: O(n)
 class Solution {
  public:
-  int getMinimumDifference(TreeNode* root) {}
+  int getMinimumDifference(TreeNode* root) {
+    vector<int> sorted;
+    inorder(root, sorted);
+    int min_diff = sorted.bach();
+    for (int i = 0; i < sorted.size() - 1; ++i) {
+      min_diff = min(sorted[i + 1] - sorted[i], min_diff);
+    }
+    return min_diff;
+  }
+  void inorder(TreeNode* root, vector<int>& sorted) {
+    if (!root) return;
+    inorder(root->left, sorted);
+    sorted.push_back(root->val);
+    inorder(root->right, sorted);
+  }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  int getMinimumDifference(TreeNode* root) {
+    min_diff = INT_MAX;
+    inorder(root);
+    return min_diff;
+  }
+  void inorder(TreeNode* node) {}
+
+ private:
+  int min_diff;
+  int* prev;
 };
