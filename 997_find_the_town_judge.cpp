cfe65bea8b7ee42b48f8e23292e5c854ba96ae0a
Universe07 <han.li@kcl.ac.uk>
2020-02-17 19:59:08 +0800

    997_find_the_town_judge.cpp

---
+++ 997_find_the_town_judge.cpp
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+  int findJudge(int N, vector<vector<int>>& trust) {
+    vector<int> degrees(N + 1);
+    for (const auto& tr : trust) {
+      --degrees[tr[0]];
+      ++degrees[tr[1]];
+    }
+    for (int i = 1; i <= N; ++i) {
+      if (degrees[i] == N - 1) return i;
+    }
+    return -1;
+  }
+};
