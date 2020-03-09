abf62e5123144b17747048d8cb8064c290ede047
Universe07 <han.li@kcl.ac.uk>
2020-03-09 22:08:30 +0800

    856_score_of_parentheses.cpp

--- 856_score_of_parentheses.cpp
+++ 856_score_of_parentheses.cpp
@@ -1,3 +1,6 @@
+// Time: O(n)
+// Space:O(1)
+
 class Solution {
  public:
   int scoreOfParentheses(string S) {
@@ -13,5 +16,26 @@ class Solution {
       }
     }
     return ans;
+  }
+};
+
+// Time: O(n)-O(n^2)
+// Space:(n)
+
+class Solution {
+ public:
+  int scoreOfParentheses(string S) {
+    return scoreCounter(S, 0, S.length() - 1);
+  }
+  int scoreCounter(const string& S, int left, int right) {
+    if (right - left == 1) return 1;
+    int b = 0;
+    for (int i = left; i < right; ++i) {
+      if (S[i] == '(') ++b;
+      if (S[i] == ')') --b;
+      if (b == 0)
+        return scoreCounter(S, left, i) + scoreCounter(S, i + 1, right);
+    }
+    return 2 * scoreCounter(S, left + 1, right - 1);
   }
 };
