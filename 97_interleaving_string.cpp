69a8e5d6573305ccca55822ff9ac30f78132af15
Universe07 <han.li@kcl.ac.uk>
2020-03-17 23:50:27 +0800

    97_interleaving_string.cpp

---
+++ 97_interleaving_string.cpp
@@ -0,0 +1,21 @@
+// Time: O(mn)
+// Space: O(mn)
+
+class Solution {
+ public:
+  bool isInterleave(string s1, string s2, string s3) {
+    int l1 = s1.length();
+    int l2 = s2.length();
+    int l3 = s3.length();
+    if (l1 + l2 != l3) return false;
+    vector<vector<int>> dp(l1 + 1, vector<int>(l2 + 1));
+    dp[0][0] = true;
+    for (int i = 0; i <= l1; ++i) {
+      for (int j = 0; j <= l2; ++j) {
+        if (i > 0) dp[i][j] |= dp[i - 1][j] && s1[i - 1] == s3[i + j - 1];
+        if (j > 0) dp[i][j] |= dp[i][j - 1] && s2[j - 1] == s3[i + j - 1];
+      }
+    }
+    return dp[l1][l2];
+  }
+};
