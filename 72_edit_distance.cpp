b0c96cf9bc9e885a919000747252a1c36fde0f1b
Universe07 <han.li@kcl.ac.uk>
2020-03-17 23:31:48 +0800

    72_edit_distance.cpp

--- 72_edit_distance.cpp
+++ 72_edit_distance.cpp
@@ -1,31 +1,47 @@
+// Time: O(l1 * l2)
+// Space: O(l1 * l2) -> O(min(l1, l2))
+
 class Solution {
  public:
   int minDistance(string word1, string word2) {
-    const int m = word1.size();
-    const int n = word2.size();
-    if (m == 0) {
-      return n;
-    }
-    if (n == 0) {
-      return m;
-    }
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-    for (int i = 0; i < m + 1; ++i) {
-      dp[i][0] = i;
-    }
-    for (int j = 0; j < n + 1; ++j) {
-      dp[0][j] = j;
-    }
+    int l1 = word1.length();
+    int l2 = word2.length();
+    d_ = vector<vector<int>>(l1 + 1, vector<int>(l2 + 1, -1));
+    return minDistance(word1, word2, l1, l2);
+  }
 
-    for (int i = 1; i <= m; ++i) {
-      for (int j = 1; j <= n; ++j) {
-        if (word1[i - 1] == word2[j - 1]) {
-          dp[i][j] = dp[i - 1][j - 1];
-        } else {
-          dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
-        }
+ private:
+  vector<vector<int>> d_;
+  int minDistance(const string& word1, const string& word2, int l1, int l2) {
+    if (l1 == 0) return l2;
+    if (l2 == 0) return l1;
+    if (d_[l1][l2] >= 0) return d_[l1][l2];
+    int ans;
+    if (word1[l1 - 1] == word2[l2 - 1])
+      ans = minDistance(word1, word2, l1 - 1, l2 - 1);
+    else
+      ans = min(minDistance(word1, word2, l1 - 1, l2 - 1),
+                min(minDistance(word1, word2, l1 - 1, l2),
+                    minDistance(word1, word2, l1, l2 - 1))) +
+            1;
+    return d_[l1][l2] = ans;
+  }
+};
+
+class Solution {
+ public:
+  int minDistance(string word1, string word2) {
+    int l1 = word1.length();
+    int l2 = word2.length();
+    vector<vector<int>> d(l1 + 1, vector<int>(l2 + 1, 0));
+    for (int i = 0; i <= l1; ++i) d[i][0] = i;
+    for (int j = 0; j <= l2; ++j) d[0][j] = j;
+    for (int i = 1; i <= l1; ++i) {
+      for (int j = 1; j <= l2; ++j) {
+        int c = (word1[i - 1] == word2[j - 1]) ? 0 : 1;
+        d[i][j] = min(d[i - 1][j - 1] + c, min(d[i - 1][j], d[i][j - 1]) + 1);
       }
     }
-    return dp[m][n];
+    return d[l1][l2];
   }
 };
