6660eda7087c1a3d48b4e25b893b7a67a6e713e3
Universe07 <han.li@kcl.ac.uk>
2020-03-19 22:30:57 +0800

    322_coin_change.cpp

--- 322_coin_change.cpp
+++ 322_coin_change.cpp
@@ -4,17 +4,15 @@
 class Solution {
  public:
   int coinChange(vector<int>& coins, int amount) {
-    vector<int> dp(amount + 1, -1);
+    vector<int> dp(amount + 1, INT_MAX);
     dp[0] = 0;
-    for (int i = 1; i <= amount; ++i) {
-      for (int j = 0; j < coins.size(); ++j) {
-        if (i - coins[j] >= 0 && dp[i - coins[j]] != -1) {
-          if (dp[i] == -1 || dp[i] > dp[i - coins[j]] + 1) {
-            dp[i] = dp[i - coins[j]] + 1;
-          }
+    for (int coin : coins) {
+      for (int i = coin; i <= amount; ++i) {
+        if (dp[i - coin] != INT_MAX) {
+          dp[i] = min(dp[i], dp[i - coin] + 1);
         }
       }
     }
-    return dp[amount];
+    return dp[amount] == INT_MAX ? -1 : dp[amount];
   }
 };
