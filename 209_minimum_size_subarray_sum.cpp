4bc17723dad7d1123ff1f61fc28b38284f37c5ba
Universe07 <han.li@kcl.ac.uk>
2020-04-13 21:10:54 +0800

    209_minimum_size_subarray_sum.cpp

---
+++ 209_minimum_size_subarray_sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+  int minSubArrayLen(int s, vector<int>& nums) {
+    int l = 0, r = 0, sum = 0, n = nums.size(), len = INT_MAX;
+    while (r < n) {
+      sum += nums[r++];
+      while (sum >= s) {
+        len = min(len, r - l);
+        sum -= nums[l--];
+      }
+    }
+    return len == INT_MAX ? 0 : len;
+  }
+};
