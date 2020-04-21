ab340cbfc20bd13f4e0dd9ee48f5572175502f00
Universe07 <han.li@kcl.ac.uk>
2020-04-21 22:04:28 +0800

    226_invert_binary_tree.cpp

---
+++ 226_invert_binary_tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  TreeNode* invertTree(TreeNode* root) {
+    stack<TreeNode*> stk;
+    stk.push(root);
+    while (!stk.empty()) {
+      auto node = stk.top();
+      stk.pop();
+      if (node) {
+        stk.push(node->left);
+        stk.push(node->right);
+        swap(node->left, node->right);
+      }
+    }
+    return root;
+  }
+};
