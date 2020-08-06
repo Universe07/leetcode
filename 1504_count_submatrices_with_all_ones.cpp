fe72fc770f2c3e6dc91cb2d60e56cb5e7bb88170
Universe07 <han.li@kcl.ac.uk>
2020-08-06 23:03:58 +0800

    1504_count_submatrices_with_all_ones.cpp

---
+++ 1504_count_submatrices_with_all_ones.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  int m, n;
+
+ public:
+  int numSubmat(vector<vector<int>>& mat) {
+    m = mat.size();
+    n = mat[0].size();
+    int count = 0;
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j) count += helper(mat, i, j);
+    return count;
+  }
+  int helper(vector<vector<int>>& mat, int a, int b) {
+    int count = 0;
+    int bound = n;
+    for (int i = a; i < m; ++i) {
+      for (int j = b; j < bound; ++j) {
+        if (mat[i][j])
+          count += 1;
+        else
+          bound = j;
+      }
+    }
+    return count;
+  }
+};
